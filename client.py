96050ed2fd1da15906ff937a1b3c7ba764716834
Philippe Mariman <pmariman@nalys-group.com>
2019-10-29 17:00:24 +0100

    wip

--- client.py
+++ client.py
@@ -8,6 +8,10 @@ socket.connect("tcp://127.0.0.1:5556")
 
 while True:
     req_str = input('>> ')
+
+    if len(req_str) == 0:
+        continue
+
     req_str = req_str.encode('utf-8')
     socket.send(req_str)
 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys
 import zmq
 
+
 help_str = \
 """
 [? | help]                                          --> <this help string>
@@ -13,8 +14,42 @@ mode:chat/req:send/user:<name>/msg:<msg>            --> mode:chat/rep:[ok|nok]
 """
 
 
+def mode_chat_parse(req_list):
+    req = req_list[1].split(sep=':')
+
+    if req == None or len(req) <= 1:
+        rep = 'mode:chat/rep:nok'
+        print('error: REQ [mode = chat, req = {}]'.format(req[0]))
+        return rep
+
+    print('mode = chat, req = {}'.format(req[1]))
+
+    if req[1] == 'login':
+        rep = 'mode:chat/rep:ok'
+    else:
+        rep = 'mode:chat/rep:nok'
+
+    return rep
+
+
 def mode_ctrl_parse(req_list):
-    return 'ok'
+    req = req_list[1].split(sep=':')
+
+    if req == None or len(req) <= 1:
+        rep = 'mode:ctrl/rep:nok'
+        print('error: REQ [mode = ctrl, req = {}]'.format(req[0]))
+        return rep
+
+    print('mode = ctrl, req = {}'.format(req[1]))
+
+    if req[1] == 'info':
+        rep = 'mode:ctrl/rep:ok'
+    elif req[1] == 'ping':
+        rep = 'mode:ctrl/rep:pong'
+    else:
+        rep = 'mode:ctrl/rep:nok'
+
+    return rep
 
 
 zctx = zmq.Context()
@@ -34,12 +69,24 @@ while True:
         continue
 
     req_list = req_str.split(sep='/')
-    _, mode = req_list[0].split(sep=':')
-    print('req mode = {}'.format(mode))
 
-    if mode == 'ctrl':
+    if len(req_list) < 2:
+        socket.send(b'nok')
+        print('error: REQ [{}]'.format(req_str))
+        continue
+
+    mode = req_list[0].split(sep=':')
+
+    if mode == None or len(mode) <= 1:
+        socket.send(b'nok')
+        print('error: REQ [{}]'.format(req_str))
+        continue
+
+    print('mode = {}'.format(mode[1]))
+
+    if mode[1] == 'ctrl':
         rep_str = mode_ctrl_parse(req_list)
-        rep_str = rep_str.decode('utf-8')
+        rep_str = rep_str.encode('utf-8')
         socket.send(rep_str)
     else:
-        socket.send(b'ok')
+        socket.send(b'nok')
