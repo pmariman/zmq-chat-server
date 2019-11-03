ac388fb8dd06c44ba5f31370f88f54b170dbc9b2
Philippe Mariman <philippe.mariman@gmail.com>
2019-11-03 19:57:45 +0100

    wip

--- server.py
+++ server.py
@@ -1,92 +1,253 @@
-import sys
+#!/usr/bin/env python3
+
+import argparse
+import http.server
+import time
+import threading
 import zmq
 
 
-help_str = \
+# constants
+HTTP_PORT=8090
+RADIO_PORT=12345
+PUB_PORT=5555
+REP_PORT=5556
+
+
+#######################################################################################################################
+
+
+# TODO create server class that contains users, sessions, ...
+# TODO capture exit signals
+
+
+#######################################################################################################################
+
+
+req_help_str = \
 """
-[? | help]                                          --> <this help string>
-mode:ctrl/req:info                                  --> mode:ctrl/rep:users=<users>,...
-mode:ctrl/req:ping                                  --> mode:ctrl/rep:ping
-mode:chat/req:login/name:<name>/passwd:<passwd>     --> mode:chat/rep:[ok|nok]
-mode:chat/req:logout/name:<name>                    --> mode:chat/rep:[ok|nok]
-mode:chat/req:post/topic:<name>/msg:<msg>           --> mode:chat/rep:[ok|nok]
-mode:chat/req:send/user:<name>/msg:<msg>            --> mode:chat/rep:[ok|nok]
-"""
+[? | help]                      --> <this help string>
+ctrl/info                       --> ctrl/users=<users>,...
+ctrl/ping                       --> ctrl/ping
+chat/login/<user>:<passwd>      --> chat/[ok|nok]
+chat/logout/<user>              --> chat/[ok|nok]
+chat/post/<topic>:<msg>         --> chat/[ok|nok]
+chat/send/<user>:<msg>          --> chat/[ok|nok]"""
 
 
-def mode_chat_parse(req_list):
-    req = req_list[1].split(sep=':')
+class replier:
+    def __init__(self, zctx, port):
+        self.zctx = zctx
+        self.port = port
+        self.open_socket()
 
-    if req == None or len(req) <= 1:
-        rep = 'mode:chat/rep:nok'
-        print('error: REQ [mode = chat, req = {}]'.format(req[0]))
+    def open_socket(self):
+        self.socket = self.zctx.socket(zmq.REP)
+        self.socket.bind('tcp://0.0.0.0:{}'.format(self.port))
+
+    def send_reply(self, rep_str):
+        raw = rep_str.encode('utf-8')
+        self.socket.send(raw)
+
+    def wait_for_request(self):
+        raw = self.socket.recv()
+        return raw.decode('utf-8')
+
+    def mode_chat_parse(self, req_list):
+        req = req_list[1]
+
+        if len(req) <= 1:
+            rep = 'chat/nok'
+            print('error: REQ [mode = chat, req = {}]'.format(req))
+            return rep
+
+        print('mode = chat, req = {}'.format(req))
+
+        if req == 'login':
+            rep = 'chat/ok'
+        else:
+            rep = 'chat/nok'
+
         return rep
 
-    print('mode = chat, req = {}'.format(req[1]))
+    def mode_ctrl_parse(self, req_list):
+        req = req_list[1]
+        if len(req) <= 1:
+            rep = 'ctrl/nok'
+            print('error: REQ format [mode = ctrl, req = {}]'.format(req))
+            return rep
 
-    if req[1] == 'login':
-        rep = 'mode:chat/rep:ok'
-    else:
-        rep = 'mode:chat/rep:nok'
+        print('mode = ctrl, req = {}'.format(req))
 
-    return rep
+        if req == 'info':
+            rep = 'ctrl/ok'
+        elif req == 'ping':
+            rep = 'ctrl/pong'
+        else:
+            rep = 'ctrl/nok'
 
-
-def mode_ctrl_parse(req_list):
-    req = req_list[1].split(sep=':')
-
-    if req == None or len(req) <= 1:
-        rep = 'mode:ctrl/rep:nok'
-        print('error: REQ [mode = ctrl, req = {}]'.format(req[0]))
         return rep
 
-    print('mode = ctrl, req = {}'.format(req[1]))
+    def parse_request(self, req_str):
+        print('recv: {}'.format(req_str))
 
-    if req[1] == 'info':
-        rep = 'mode:ctrl/rep:ok'
-    elif req[1] == 'ping':
-        rep = 'mode:ctrl/rep:pong'
-    else:
-        rep = 'mode:ctrl/rep:nok'
+        if req_str in ('?', 'help'):
+            rep_str = req_help_str
+            return rep_str
 
-    return rep
+        req_list = req_str.split(sep='/')
+
+        if len(req_list) < 2:
+            print('error: REQ format [{}]'.format(req_str))
+            return 'nok'
+
+        mode = req_list[0]
+
+        if len(mode) <= 1:
+            print('error: REQ format [{}]'.format(req_str))
+            return 'nok'
+
+        print('mode = {}'.format(mode))
+
+        if mode == 'ctrl':
+            rep_str = self.mode_ctrl_parse(req_list)
+            return rep_str
+
+        return 'nok'
+
+    def thread_req(self):
+        while True:
+            req_str = self.wait_for_request()
+            rep_str = self.parse_request(req_str)
+            self.send_reply(rep_str)
+
+    def start(self):
+        self.tid = threading.Thread(target=self.thread_req, daemon=True)
+        self.tid.start()
 
 
-zctx = zmq.Context()
-socket = zctx.socket(zmq.REP)
+#######################################################################################################################
 
-socket.bind("tcp://127.0.0.1:5556")
 
-while True:
-    req_str = socket.recv()
-    req_str = req_str.decode('utf-8')
+class publisher:
+    def __init__(self, zctx, port):
+        self.zctx = zctx
+        self.port = port
+        self.open_socket()
 
-    print('recv: {}'.format(req_str))
+    def open_socket(self):
+        self.socket = self.zctx.socket(zmq.PUB)
+        self.socket.bind('tcp://0.0.0.0:{}'.format(self.port))
 
-    if req_str in ('?', 'help'):
-        rep_str = help_str.encode('utf-8')
-        socket.send(rep_str)
-        continue
+    def send(self, topic, msg):
+        data = "chat/{}/{}".format(topic, msg)
+        self.socket.send(data.encode('utf-8'))
 
-    req_list = req_str.split(sep='/')
 
-    if len(req_list) < 2:
-        socket.send(b'nok')
-        print('error: REQ [{}]'.format(req_str))
-        continue
+#######################################################################################################################
 
-    mode = req_list[0].split(sep=':')
 
-    if mode == None or len(mode) <= 1:
-        socket.send(b'nok')
-        print('error: REQ [{}]'.format(req_str))
-        continue
+http_content = \
+"""<html>
+<head><title>501 Not Implemented</title></head>
+<body bgcolor="white">
+<center><h1>501 Not Implemented</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+"""
 
-    print('mode = {}'.format(mode[1]))
 
-    if mode[1] == 'ctrl':
-        rep_str = mode_ctrl_parse(req_list)
-        rep_str = rep_str.encode('utf-8')
-        socket.send(rep_str)
-    else:
-        socket.send(b'nok')
+class SimpleReply(http.server.BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(501)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _html(self):
+        return http_content.encode("utf8")
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._html())
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write(self._html("POST!"))
+
+    def log_message(self, format, *args):
+        return
+
+
+class dummy_http:
+    def __init__(self, port):
+        self.port = port
+
+    def thread_http(self):
+        server_address = ("0.0.0.0", self.port)
+        print('starting httpd server on {}'.format(server_address))
+        httpd = http.server.HTTPServer(server_address, SimpleReply)
+        httpd.serve_forever()
+
+    def start(self):
+        self.tid = threading.Thread(target=self.thread_http, daemon=True)
+        self.tid.start()
+
+
+#######################################################################################################################
+
+
+def timer_callback(pub):
+    pub.send('foo', 'bar')
+    threading.Timer(2, timer_callback, args=(pub,)).start()
+
+
+#######################################################################################################################
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument('-h', '--http', dest='http_port', type=int, nargs='?', const=HTTP_PORT,
+            help='enable http socket on port (default: {})'.format(HTTP_PORT))
+    parser.add_argument('-p', '--pub', dest='pub_port', type=int, nargs='?', const=PUB_PORT, default=PUB_PORT,
+            help='enable publish socket on port (default: {})'.format(PUB_PORT))
+    parser.add_argument('-r', '--rep', dest='rep_port', type=int, nargs='?', const=REP_PORT, default=REP_PORT,
+            help='enable publish socket on port (default: {})'.format(REP_PORT))
+
+    parser.add_argument("-v", "--verbose", dest='verbose', action='store_true', help="verbose mode")
+    parser.add_argument('--help', action='help', help='show this help message and exit')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    print('args: {}'.format(args))
+
+    if args.http_port != None:
+        web_dummy = dummy_http(args.http_port)
+        web_dummy.start()
+
+    zctx = zmq.Context()
+
+    if args.pub_port != None:
+        pub = publisher(zctx, args.pub_port)
+        pub = timer_callback(pub)
+
+    if args.rep_port != None:
+        rep = replier(zctx, args.rep_port)
+        rep.start()
+
+    time.sleep(99999)
+
+
+if __name__ == "__main__":
+    main()
--- test-sub.py
+++ test-sub.py
@@ -4,13 +4,13 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 
-socket.connect ("tcp://172.17.0.3:5555")
+socket.connect('tcp://127.0.0.1:5555')
 
-topicfilter = b"test:"
+topicfilter = b"chat"
 socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
 
 while True:
     string = socket.recv()
     string = string.decode('utf-8')
-    topic, msg = string.split(sep=':')
+    chat, topic, msg = string.split(sep='/')
     print("recv: {}:{}".format(topic, msg))
